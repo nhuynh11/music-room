43c6556cc4d9ec826153f363e956ac05348d2a79
nphuynh11 <nphuynh11@gmail.com>
2021-01-21 21:19:24 -0500

    adding model view and serializer for room

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Room
+
+# translate the room into a json response
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
